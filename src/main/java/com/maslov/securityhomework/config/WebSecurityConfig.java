81cc3f4faf9005bce3343f2d6581142fb200dbd4
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-09-11 17:32:31 +0500

    delete csrf

--- src/main/java/com/maslov/securityhomework/config/WebSecurityConfig.java
+++ src/main/java/com/maslov/securityhomework/config/WebSecurityConfig.java
@@ -22,25 +22,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                    .antMatchers("/", "/registration").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .permitAll();
+            .csrf()
+                .disable()
+            .authorizeRequests()
+                .antMatchers("/", "/registration").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+                .logout()
+                .permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select name, password, active from usr where name=?")
-                .authoritiesByUsernameQuery("select u.name, ur.roles from usr u inner join user_role ur " +
-                        "on u.id = ur.user_id where u.name=?");
+            .dataSource(dataSource)
+            .passwordEncoder(NoOpPasswordEncoder.getInstance())
+            .usersByUsernameQuery("select name, password, active from usr where name=?")
+            .authoritiesByUsernameQuery("select u.name, ur.roles from usr u inner join user_role ur " +
+                    "on u.id = ur.user_id where u.name=?");
     }
 }
