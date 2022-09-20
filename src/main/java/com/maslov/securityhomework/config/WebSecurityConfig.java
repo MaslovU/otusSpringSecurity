05bbb3f025965297b2b8c40199342380cda2ab8d
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-09-20 15:04:50 +0500

    polish

--- src/main/java/com/maslov/securityhomework/config/WebSecurityConfig.java
+++ src/main/java/com/maslov/securityhomework/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.maslov.securityhomework.config;
 
-import com.maslov.securityhomework.domain.Role;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- src/main/java/com/maslov/securityhomework/controller/BookController.java
+++ src/main/java/com/maslov/securityhomework/controller/BookController.java
@@ -4,16 +4,10 @@ import com.maslov.securityhomework.domain.Book;
 import com.maslov.securityhomework.domain.Comment;
 import com.maslov.securityhomework.model.BookModel;
 import com.maslov.securityhomework.service.BookService;
-import org.springframework.boot.Banner;
-import org.springframework.jdbc.core.SqlReturnType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
